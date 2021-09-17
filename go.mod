17c99cc5666c8b221efda8b6932e3c169fa10428
king <valvuong2201@gmail.com>
2021-09-17 21:07:57 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module vuong/hello
+
+go 1.17
+
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+)
---
+++ hello.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"html/template"
+	"math"
+	"net/http"
+	"net/url"
+	"os"
+	"log"
+	"github.com/joho/godotenv"
+	"time"
+	"strconv"
+	"bytes"
+	"vuong/hello/news"
+)
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+var newsapi *news.Client
+
+type Search struct {
+	Query string
+	NextPage int
+	TotalPages int
+	Results *news.Results
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request)  {
+	buf := bytes.Buffer{}
+	err := tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	buf.WriteTo(w)
+}
+
+func searchHandler(newsapi *news.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request)  {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		params := u.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
+
+		result, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		nextPage, err := strconv.Atoi(page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		search := &Search{
+			Query: searchQuery,
+			NextPage: nextPage,
+			TotalPages: int(math.Ceil(float64(result.TotalResults) / float64(newsapi.PageSize))),
+			Results: result,
+		}
+
+		if ok := !search.IsLastPage(); ok {
+			search.NextPage++
+		}
+
+		buf := &bytes.Buffer{}
+		err = tpl.Execute(buf, search)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		buf.WriteTo(w)
+	}
+}
+
+func (s *Search) IsLastPage() bool  {
+	return s.NextPage >= s.TotalPages
+}
+
+func (s *Search) CurrentPage() int {
+	if s.NextPage == 1 {
+		return s.NextPage
+	}
+	return s.NextPage - 1
+}
+
+func (s *Search) PreviousPage() int  {
+	return s.CurrentPage() - 1
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Env: apiKey must be set")
+	}
+
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsapi := news.NewClient(myClient, apiKey, 20)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	fs := http.FileServer(http.Dir("assets"))
+	mux := http.NewServeMux()
+
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/search", searchHandler(newsapi))
+	http.ListenAndServe(":" + port, mux)
+}
